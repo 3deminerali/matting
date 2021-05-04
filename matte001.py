c07a30e14276fe62825d9e599de17068073d0c40
3deminerali <82756448+3deminerali@users.noreply.github.com>
2021-05-04 16:16:39 +0200

    Update matte001.py

--- matte001.py
+++ matte001.py
@@ -1,3 +1,4 @@
+#AI background matting based on https://github.com/PeterL1n/BackgroundMattingV2 . Look at their code, development and their license https://github.com/PeterL1n/BackgroundMattingV2/blob/master/LICENSE.
 import os
 import sys
 print(os.getcwd())
